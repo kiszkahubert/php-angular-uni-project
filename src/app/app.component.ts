f0e37969646a69c350c5f53e6a266eaef7c6b285
Hubert <kiszkahub@gmail.com>
2024-11-27 15:27:37 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginPageComponent } from "./auth/login-page/login-page.component";
+import { RegisterPageComponent } from "./auth/register-page/register-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginPageComponent],
+  imports: [RouterOutlet, LoginPageComponent, RegisterPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/auth/register-page/register-page.component.ts
+++ src/app/auth/register-page/register-page.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css'
 })
 export class RegisterPageComponent {
+  username: string = '';
+  password: string = '';
+  emailExists: boolean = false;
+  isPasswordIncorrect: boolean = false;
 
+  onSubmit(){
+    
+  }
 }
